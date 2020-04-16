a65d02a9360cec5ac56e769f58ab77698af88e73
Junya Oiwa <0920oiwa@gmail.com>
2020-04-16 10:44:16 +0900

    [add]お問い合わせページにフォームを追加

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="contact">
     <Contents :content="content" />
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe_8zsc81TUDNHT6sGWApnYBZyW5M_f2OeNH8e1IK5sTtI0zg/viewform?embedded=true" class="contact-form" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
   </div>
 </template>
 
@@ -20,5 +21,9 @@ export default {
 </script>
 
 <style>
-
+.contact-form {
+  width: 100%;
+  height: 1500px;
+  margin-top: 30px;
+}
 </style>
