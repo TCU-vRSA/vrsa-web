2ec452d2c4431cd1e323bef5b2604ad76e17fbd1
Junya Oiwa <0920oiwa@gmail.com>
2020-04-16 11:03:22 +0900

    [add]metaタグをセット

--- pages/article/_id.vue
+++ pages/article/_id.vue
@@ -7,8 +7,29 @@
 <script>
 import Contents from '~/components/layouts/Contents.vue';
 export default {
+  head() {
+    return {
+      title: this.content.title + ' | TCU vRSA - 都市大非公認学生リモートサポートチーム',
+      meta: [
+        { hid: 'description', name: 'description', content: this.content.description },
+        { hid: 'og:title', name: 'og:title', content: this.content.title + ' | TCU vRSA - 都市大非公認学生リモートサポートチーム' },
+        { hid: 'og:url', name: 'og:url', content: this.getUrl() },
+        { hid: 'og:type', name: 'og:type', content: 'website' },
+        { hid: 'og:description', name: 'og:description', content: '東京都市大学非公認・リモート学生アシスタントチーム「vRSA」の公式Webサイトです。' },
+        { hid: 'og:image', property: 'og:image', content: 'https://tcu-vrsa.net/img/ogp.png' },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'TCU vRSA - 都市大非公認学生リモートサポートチーム' },
+        { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' },
+      ]
+    }
+  },
   components: {
     Contents,
+  },
+  methods: {
+    getUrl: function() {
+      return `https://tcu-vrsa.net${this.$route.path}`
+    },
   },
   async asyncData({ app, params }) {
     const response = await app.$axios.$get(`https://api.tcu-vrsa.net/articles/${params.id}`);
--- pages/qa.vue
+++ pages/qa.vue
@@ -7,6 +7,22 @@
 <script>
 import Contents from '~/components/layouts/Contents.vue';
 export default {
+  head() {
+    return {
+      title: this.content.title + ' | TCU vRSA - 都市大非公認学生リモートサポートチーム',
+      meta: [
+        { hid: 'description', name: 'description', content: '東京都市大学非公認・リモート学生アシスタントチーム「vRSA」の公式Webサイトです。' },
+        { hid: 'og:title', name: 'og:title', content: this.content.title + ' | TCU vRSA - 都市大非公認学生リモートサポートチーム' },
+        { hid: 'og:url', name: 'og:url', content: 'https://tcu-vrsa.net/qa' },
+        { hid: 'og:type', name: 'og:type', content: 'website' },
+        { hid: 'og:description', name: 'og:description', content: '東京都市大学非公認・リモート学生アシスタントチーム「vRSA」の公式Webサイトです。' },
+        { hid: 'og:image', property: 'og:image', content: 'https://tcu-vrsa.net/img/ogp.png' },
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'TCU vRSA - 都市大非公認学生リモートサポートチーム' },
+        { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' },
+      ]
+    }
+  },
   components: {
     Contents,
   },
